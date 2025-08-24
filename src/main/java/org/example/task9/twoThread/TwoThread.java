345787e9d5ff0172c4e330ec8939ee11b8a740f2
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-08-24 15:23:04 +0300

    .

--- src/main/java/org/example/task9/twoThread/TwoThread.java
+++ src/main/java/org/example/task9/twoThread/TwoThread.java
@@ -1,4 +1,34 @@
 package org.example.task9.twoThread;
 
 public class TwoThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("A");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("B");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        );
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+
 }
