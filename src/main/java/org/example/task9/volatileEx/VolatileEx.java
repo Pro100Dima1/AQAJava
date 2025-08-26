dc8780b865fc86f8c1777738442da0313e88938f
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-08-26 14:11:27 +0300

    .

--- src/main/java/org/example/task9/volatileEx/VolatileEx.java
+++ src/main/java/org/example/task9/volatileEx/VolatileEx.java
@@ -1,5 +1,21 @@
 package org.example.task9.volatileEx;
 
-public class VolatileEx  {
+public class VolatileEx {
+    static volatile boolean isStoping = false;
+    static int counter = 0;
 
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            while (!isStoping) {
+                counter++;
+
+            }
+        });
+
+        t1.start();
+        //System.out.println(counter);
+        Thread.sleep(2000);
+        isStoping = true;
+        t1.join();
+    }
 }
