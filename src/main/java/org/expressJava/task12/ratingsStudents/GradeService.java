0577fdff6dfd988c92fea0a5152cef7de5623c01
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-10-12 22:56:36 +0300

    .

--- src/main/java/org/expressJava/task12/ratingsStudents/GradeService.java
+++ src/main/java/org/expressJava/task12/ratingsStudents/GradeService.java
@@ -1,4 +1,27 @@
 package org.expressJava.task12.ratingsStudents;
 
-public class GradeService {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradeService<T extends Number> {
+    private final List<StudentGrade<T>> gradeArray = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public synchronized void addGrade(StudentGrade<T> grade) throws InvalidGradeException {
+        if (grade.getGrade().intValue() < 0) {
+            throw new InvalidGradeException("The rating cannot be negative");
+        }
+        gradeArray.add(grade);
+    }
+
+    public synchronized int averageGrade(String object) {
+        return (int) gradeArray.stream()
+                .filter(s -> s.getSubject().equalsIgnoreCase(object))
+                .mapToInt(s -> s.getGrade().intValue())
+                .average()
+                .orElseThrow(() -> new IllegalArgumentException("An invalid argument was entered in the calculation"));
+    }
 }
---
+++ src/test/java/complexTasks/GradeServiceTest.java
@@ -0,0 +1,13 @@
+package complexTasks;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GradeServiceTest {
+
+    @Test
+    @DisplayName("Checking the addition of a student to the list")
+    public void checkAddStudentInList(){
+
+    }
+}
