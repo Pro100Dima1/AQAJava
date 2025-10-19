02cbad0a184a652e5d90ae6caf56ac14027b71df
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-10-19 18:28:01 +0300

    .

--- src/main/java/org/expressJava/task12/ratingsMovie/MovieService.java
+++ src/main/java/org/expressJava/task12/ratingsMovie/MovieService.java
@@ -1,4 +1,15 @@
 package org.expressJava.task12.ratingsMovie;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class MovieService {
-}
+    HashMap<Movie, List<Rating<? extends Number>>> listMovie = new HashMap<>();
+
+    public synchronized void addRating(Rating<? extends Number> rating) {
+        if (rating.getValue().doubleValue() < 1 || rating.getValue().doubleValue() > 10) {
+            throw new IllegalArgumentException("Оценка должна быть от 1 до 10");
+        }
+    }
+
+    }
