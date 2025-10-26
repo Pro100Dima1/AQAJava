6eedd7114ecbb070b1fc12dbcbdc15c320ec4195
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-10-27 00:03:16 +0300

    .

--- src/main/java/org/expressJava/task12/inventory/InventoryService.java
+++ src/main/java/org/expressJava/task12/inventory/InventoryService.java
@@ -1,4 +1,24 @@
 package org.expressJava.task12.inventory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class InventoryService {
+    boolean isInventoryOpem = false;
+    public Map<String, List<Product>> products = new HashMap<>();
+
+    public void addProduct(Product product) {
+        if (!isInventoryOpem) {
+            return;
+        }
+        else {
+            products.putIfAbsent(product.getCategory(), new ArrayList<>());
+        }
+    }
+
+    public Product getProductByCategory(){
+
+    }
 }
--- src/main/java/org/expressJava/task12/inventory/Product.java
+++ src/main/java/org/expressJava/task12/inventory/Product.java
@@ -1,4 +1,25 @@
 package org.expressJava.task12.inventory;
 
 public class Product {
+    private final int price;
+    private final String name;
+    private final String category;
+
+    public Product(int price, String name, String category) {
+        this.price = price;
+        this.name = name;
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
