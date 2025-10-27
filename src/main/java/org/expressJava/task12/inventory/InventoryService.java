0fa3d5f9ec78fc00a8ac1b7ef7ed707230adedaa
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-10-27 23:00:44 +0300

    .

--- src/main/java/org/expressJava/task12/inventory/InventoryService.java
+++ src/main/java/org/expressJava/task12/inventory/InventoryService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InventoryService {
     boolean isInventoryOpem = false;
@@ -12,13 +13,24 @@ public class InventoryService {
     public void addProduct(Product product) {
         if (!isInventoryOpem) {
             return;
-        }
-        else {
+        } else {
             products.putIfAbsent(product.getCategory(), new ArrayList<>());
         }
     }
 
-    public Product getProductByCategory(){
+    public synchronized Product getProductByCategory(String category) throws OutOfStockException {
+        List<Product> product = products.get(category);
+        if (products == null || products.isEmpty()) {
+            throw new OutOfStockException("Товары в категории \"" + category + "\" отсутствуют");
+        }
+        return product.remove(0); // извлекаем первый доступный
+    }
 
+    public List<Product> filterByPrice(double minPrice) {
+        return products.values().stream().flatMap(List::stream).filter(p -> p.getPrice() >= minPrice).collect(Collectors.toList());
+    }
+
+    public List<Product> getAllByCategory(String category) {
+        return new ArrayList<>(products.getOrDefault(category, List.of()));
     }
 }
